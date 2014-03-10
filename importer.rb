4e9cbf043f68560b7346704385143b0d04b298da
Jon Fuller <jcfuller@sep.com>
2014-03-10 09:26:47 -0400

    creates projects!

--- importer.rb
+++ importer.rb
@@ -1,5 +1,5 @@
 class Importer
-  attr_accessor :user_hash, :gitlab
+  attr_accessor :user_hash, :group_hash, :project_hash, :gitlab
 
   def initialize(user_hash, group_hash, project_hash, repo_dir, gitlab)
     @user_hash = user_hash
@@ -29,7 +29,7 @@ class Importer
       .reject{|u| alums.has_key?(u['email'])}
       .reject{|u| u['email'] == nil}
       .reject{|u| u['email'] == 'cruisecontrol@sep.com'}
-      .take(3)
+      .take(3)  # REMOVE THIS AFTER TESTING
       .each_with_index do |u, i|
         email = garble_email(u['email'], i)
         p u
@@ -40,21 +40,59 @@ class Importer
   end
 
   def load_ssh_keys
-    @gitlab.users.drop(3).each do |u|
-      puts "loading keys for: #{u.username}" if @verbose
-      sudo(u.id) do
-        gitorious_user = @user_hash.find{|h| get_username(h['email']) == u.username}
-        next unless gitorious_user
-        gitorious_user['ssh_keys'].uniq.each do |k|
-          #key = k.split(' ')[0...-1].join(' ')
-          #title = "whatever#{rand.to_s[2..8]}"
-          title = k.split(' ').last
-          key = k
-          puts "  adding key: #{title} - #{key}" if @verbose
-          @gitlab.create_ssh_key(title, key)
+    @gitlab
+      .users
+      .drop(3)
+      .each do |u|
+        puts "loading keys for: #{u.username}" if @verbose
+        sudo(u.id) do
+          gitorious_user = @user_hash.find{|h| get_username(h['email']) == u.username}
+          next unless gitorious_user
+          gitorious_user['ssh_keys'].uniq.each do |k|
+            title = k.split(' ').last
+            key = k
+            puts "  adding key: #{title} - #{key}" if @verbose
+            @gitlab.create_ssh_key(title, key)
+          end
         end
       end
+  end
+
+  def create_groups
+    #group = create_group(name, path)
+    #add_group_member(group.id, user.id, 50 or "Master")
+    
+  end
+
+  def create_projects
+    fallback_user = {username: 'jcfuller', id: 2}
+    gitlab_users = @gitlab.users.inject({}){|memo, obj| memo[obj.username] = obj.id; memo}
+
+    @project_hash.each do |project|
+      puts "creating project #{project['title']}" if @verbose
+
+      existing_users = project['repositories']
+        .map{|r| r['committers']}
+        .flatten
+        .uniq
+        .reject{|p| not_there = !(gitlab_users.has_key?(p)); puts "  ignoring committer #{p}, not in gitlab." if not_there && @verbose; not_there}
+        .map{|u| {username: u, id: gitlab_users[u]}}
+
+      first_user = existing_users.first || fallback_user
+      other_users = existing_users.drop(1)
+
+      new_project = @gitlab.create_project(project['title'], {description: project['description'], wiki_enabled: true, wall_enabled: true, issues_enabled: true, snippets_enabled: true, merge_requests_enabled: true, public: true, user_id: first_user[:id]})
+      other_users.each do |u|
+        puts "  adding committer #{u[:username]}"
+        @gitlab.add_team_member(new_project.id, u[:id], 50)
+      end
     end
+    # create projects owned by users
+      # add users to project
+
+    # create projects owned by groups
+      # transfer
+      # add users to project
   end
 
   def sudo(id)
