dc260bcf77e89f5603e6db7ada5d644b6be49a3d
Jon Fuller <jcfuller@sep.com>
2014-03-13 21:30:58 -0400

    a little refactoring

--- importer.rb
+++ importer.rb
@@ -58,58 +58,75 @@ class Importer
       end
   end
 
-  def create_projects
-    fallback_user = {username: 'root', id: 1}
+  def add_users(gitlab_group, gitorious_project)
     gitlab_users = @gitlab.users.inject({}){|memo, obj| memo[obj.username] = obj.id; memo}
+
+    fallback_user = {username: 'root', id: 1}
+
+    existing_users = gitorious_project['repositories']
+      .map{|r| r['committers']}
+      .flatten
+      .uniq
+      .reject{|p| not_there = !(gitlab_users.has_key?(p)); puts "  ignoring committer #{p}, not in gitlab." if not_there && @verbose; not_there}
+      .map{|u| {username: u, id: gitlab_users[u]}}
+
+    (existing_users + [fallback_user]).each do |u|
+      puts "  adding user to group - #{u[:username]}" if @verbose
+      @gitlab.add_group_member(gitlab_group.id, u[:id], 50)
+    end
+  end
+
+  def add_repos(gitlab_group, gitorious_project)
+    fallback_user = {username: 'root', id: 1}
+
+    gitorious_project['repositories'].map do |repo|
+      puts "  adding project/repo to group - #{repo['name']}" if @verbose
+
+      description = repo['description'].empty? ? repo['name'] : repo['description']
+      new_project = @gitlab.create_project(
+        repo['name'],
+        {description: description, wiki_enabled: true, wall_enabled: true, issues_enabled: true, snippets_enabled: true, merge_requests_enabled: true, public: true, user_id: fallback_user[:id]})
+      @gitlab.transfer_project_to_group(gitlab_group.id, new_project.id)
+    end
+  end
+
+  def create_projects
 
     @project_hash.each do |project|
       puts "creating group #{project['title']}" if @verbose
 
-      existing_users = project['repositories']
-        .map{|r| r['committers']}
-        .flatten
-        .uniq
-        .reject{|p| not_there = !(gitlab_users.has_key?(p)); puts "  ignoring committer #{p}, not in gitlab." if not_there && @verbose; not_there}
-        .map{|u| {username: u, id: gitlab_users[u]}}
-
       group = @gitlab.create_group(project['title'], project['slug'])
-      (existing_users + [fallback_user]).each do |u|
-        puts "  adding user to group - #{u[:username]}" if @verbose
-        @gitlab.add_group_member(group.id, u[:id], 50)
+      add_users(group, project)
+      add_repos(group, project)
+      @gitlab.projects.each do |repo|
+        push_repo(repo, project_hash)
       end
-      project['repositories'].map do |repo|
-        puts "  adding project/repo to group - #{repo['name']}" if @verbose
-
-        new_project = @gitlab.create_project(repo['name'], {description: repo['description'], wiki_enabled: true, wall_enabled: true, issues_enabled: true, snippets_enabled: true, merge_requests_enabled: true, public: true, user_id: fallback_user[:id]})
-        @gitlab.transfer_project_to_group(group.id, new_project.id)
-      end
-
     end
+  end
 
-    @gitlab.projects.each do |project|
-      old_repo = project_hash
-        .map{|p| p['repositories'].map{|r| {project: p['title'], repo: r['name'], p: p}}}
-        .flatten
-        .find{|p| p[:repo] == project.name && p[:project] == project.namespace.name}
+  def push_repo(gitlab_project, gitorious_projects)
+    old_repo = gitorious_projects
+      .map{|p| p['repositories'].map{|r| {project: p['title'], repo: r['name'], p: p}}}
+      .flatten
+      .find{|p| p[:repo] == gitlab_project.name && p[:project] == gitlab_project.namespace.name}
 
-      dir = File.join(@repo_dir, old_repo[:p]['slug'], old_repo[:repo])
+    dir = File.join(@repo_dir, old_repo[:p]['slug'], old_repo[:repo])
 
-      if Rugged::Repository.new(dir).empty?
-        puts "skipping empty repo #{project.name}" if @verbose
-        next
-      end
-      puts "going to push repo for project #{project.name} (#{old_repo[:project]} = #{old_repo[:repo]})" if @verbose
-      puts "local repo: #{dir} - exists: #{Dir.exist?(dir)}" if @verbose
-      puts "remote repo: #{project.ssh_url_to_repo}"
-      Dir.chdir(dir) do
-        url = project.ssh_url_to_repo.gsub(ENV['GITLAB_HOST'], ENV['GITLAB_IP']) # REMOVE THIS!
-        puts "pushing to #{url}" if @verbose
-        `git remote rm gitlab`
-        `git remote add gitlab #{url}`
-        `git push gitlab --all`
-      end
-      puts "*"*80 if @verbose
+    if Rugged::Repository.new(dir).empty?
+      puts "skipping empty repo #{gitlab_project.name}" if @verbose
+      return
     end
+    puts "going to push repo for project #{gitlab_project.name} (#{old_repo[:project]} = #{old_repo[:repo]})" if @verbose
+    puts "local repo: #{dir} - exists: #{Dir.exist?(dir)}" if @verbose
+    puts "remote repo: #{gitlab_project.ssh_url_to_repo}"
+    Dir.chdir(dir) do
+      url = gitlab_project.ssh_url_to_repo.gsub(ENV['GITLAB_HOST'], ENV['GITLAB_IP']) # REMOVE THIS!
+      puts "pushing to #{url}" if @verbose
+      `git remote rm gitlab`
+      `git remote add gitlab #{url}`
+      `git push gitlab --all`
+    end
+    puts "*"*80 if @verbose
   end
 
   def sudo(id)
